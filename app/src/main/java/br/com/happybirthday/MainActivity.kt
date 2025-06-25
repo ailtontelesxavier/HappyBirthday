d0051eeda0994ccedc6f1bb89fb2228bd77769fe
Ailton teles xavier <atxshared@gmail.com>
2025-06-25 10:40:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/happybirthday/MainActivity.kt
@@ -0,0 +1,49 @@
+package br.com.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Surface (modifier = Modifier.fillMaxSize()) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message
+    )
+
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true, device = "id:pixel_3a",
+)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+    GreetingText(message = "Happy Birthday Sam!")
+    }
+}
